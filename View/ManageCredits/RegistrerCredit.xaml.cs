a02cc2870bcb05b16aa39b2bab5e30a212369316
Foforfr0 <foforfr007@gmail.com>
2024-10-12 19:26:37 -0600

    Registrro de solicitud de crédito

--- View/ManageCredits/RegistrerCredit.xaml.cs
+++ View/ManageCredits/RegistrerCredit.xaml.cs
@@ -1,4 +1,5 @@
 ﻿using System.Windows;
+using System.Windows.Input;
 
 namespace FinanciaRed.View.ManageCredits {
     /// <summary>
@@ -10,11 +11,23 @@ namespace FinanciaRed.View.ManageCredits {
         }
 
         public void ClickCancel (object sender, RoutedEventArgs e) {
-
+            MessageBoxResult result = MessageBox.Show (
+                "¿Está seguro de cancelar?\nNo se podrán recuperar los datos.",
+                "Cancelar registro.",
+                MessageBoxButton.YesNo
+            );
+            if (result == MessageBoxResult.Yes)
+                this.Close ();
         }
 
         public void ClickContinueStage2 (object sender, RoutedEventArgs e) {
         
+        }
+
+        private void PreviewTextInput_OnlyNumbers (object sender, TextCompositionEventArgs e) {
+            if (!char.IsDigit (e.Text, 0)) {
+                e.Handled = true;
+            }
         }
     }
 }
